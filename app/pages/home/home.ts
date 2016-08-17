413619ca58ef9f08f5a7a2708973717f1b642cc2
Gabriel Del Valle <gabrielmdvl@gmail.com>
2016-08-17 19:45:47 +0000

    App almost complete

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,53 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
-
+import {ModalController, NavController} from 'ionic-angular';
+import {AddItemPage} from '../add-item/add-item';
+import {ItemDetailPage} from '../item-detail/item-detail';
+import {Data} from '../../providers/data/data';
+ 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
-
+ 
+  private items = [];
+ 
+  constructor(private nav: NavController, private dataService: Data, private modalCtrl: ModalController) {
+ 
+    this.dataService.getData().then((todos) => {
+ 
+        if(todos){
+            this.items = JSON.parse(todos); 
+        }
+ 
+    });
+ 
   }
+ 
+  addItem(){
+ 
+    let addModal = this.modalCtrl.create(AddItemPage);
+ 
+    addModal.onDidDismiss((item) => {
+ 
+      if(item){
+        this.saveItem(item);
+      }
+ 
+    });
+ 
+    addModal.present();
+ 
+  }
+ 
+  saveItem(item){
+    this.items.push(item);
+    this.dataService.save(this.items);
+  }
+ 
+  viewItem(item){
+    this.nav.push(ItemDetailPage, {
+      item: item
+    });
+  }
+ 
 }
